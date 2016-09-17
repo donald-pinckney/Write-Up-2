fda157f0a31f66db7214e431bd5c8e563f904fd0
Donald Pinckney <donald_pinckney@icloud.com>
2016-09-17 15:52:20 -0700

    Delete gen.sh

--- testing/gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tdir=testing/problem3
-for i in `seq 6 20`; do
-    echo $i | swift testing/gen.swift > $tdir/$i.input
-    cat $tdir/$i.input | swift testing/genc.swift > $tdir/$i.correct
-done
