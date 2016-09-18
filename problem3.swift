73790db12a9ccd055e10bd8cb6c030579bf014bb
Donald Pinckney <donald_pinckney@icloud.com>
2016-09-17 19:19:26 -0700

    Update problem3.swift

--- problem3.swift
+++ problem3.swift
@@ -1,21 +1,3 @@
 // See problem3.README
 
 let n = Int(readLine()!)!
-
-var i = 2
-var foundDivisor = false
-while i < n {
-
-	if n % i == 0 {
-		foundDivisor = true
-		break
-	}
-
-	i += 1
-}
-
-if n <= 1 || foundDivisor {
-	print(false)
-} else {
-	print(true)
-}
