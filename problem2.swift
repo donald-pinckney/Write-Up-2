5d0265d992eef65ef69c4a435d08a8032414518c
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-23 03:36:25 +0000

    Solve problem 2

--- problem2.swift
+++ problem2.swift
@@ -2,7 +2,12 @@
 import Foundation
 
 let y = Double(readLine()!)!
-
+var x = y - 0.5
+var i = 0
+while i <= 11 {
+    x = 0.5 * (x + y / x)
+    i = i + 1
+}
 // Use this to output only 4 decimal digits:
 print(String.localizedStringWithFormat("%.4f", x))
 
