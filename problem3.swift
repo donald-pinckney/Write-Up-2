1ecfd3c94b446fe7a174d2697aa5c2eb27373a84
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-23 03:54:39 +0000

    Started problem 3

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,18 @@
 // See problem3.README
-
+import Foundation
 let n = Int(readLine()!)!
+var i = 3
+var isPrime = true
+if n % 2 != 0{
+    while (i < n) && (isPrime == true) {
+        if n % i == 0 {
+            isPrime = false
+            }
+        i = i + 2
+        } else {
+            isPrime = true        
+        }
+} else {
+ isPrime = false
+}
+print(isPrime)
