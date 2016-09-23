e85a965f5ea5a7e75ccc4c9a3129110aeadc363d
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-23 06:27:53 +0000

    Solve problem 3 (Need work on case 5)

--- problem3.swift
+++ problem3.swift
@@ -1,18 +1,19 @@
 // See problem3.README
 import Foundation
 let n = Int(readLine()!)!
-var i = 3
+var i = 2
 var isPrime = true
-if n % 2 != 0{
-    while (i < n) && (isPrime == true) {
-        if n % i == 0 {
-            isPrime = false
-            }
-        i = i + 2
-        } else {
-            isPrime = true        
-        }
-} else {
+testPrime: while i < (n - 2) {
+    if n % i == 0 {
+        isPrime = false
+        break testPrime
+    } else {
+        i = i + 1 
+    }
+}
+if n == 0 || n == 1 {
  isPrime = false
 }
+
+
 print(isPrime)
