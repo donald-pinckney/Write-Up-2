35c97be3640d1f26bdbebafabd40e9d7618b0bcc
cosbe <j.liu484@gmail.com>
2016-09-24 22:43:17 +0000

    Solved problem 1 and 3

--- problem1.swift
+++ problem1.swift
@@ -3,25 +3,25 @@
 let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
 var i = input1
-var e = input2
 var sum = 0
 
-if input1 > input2 { 
-    while e <= input1 { 
-        sum = sum + e
-        e = e + 1
-    print (sum) 
-}} 
-
-if input1 < input2 {
-    while i <= input2 {
+if input1 < input2 { 
+    while i <= input2 { 
         sum = sum + i
         i = i + 1
-    print (sum)
-} }
-
-
-if input1 == input2 { 
-    print ( input1 ) 
+    }
 } 
+
+else if input1 > input2 {
+    while i >= input2 {
+        sum = sum + i
+        i = i - 1
+    }   
+} 
+
+else { 
+    sum = input1     
+} 
+
+print (sum)
 
--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,25 @@
 // See problem3.README
 
+import Foundation
+
 let n = Int(readLine()!)!
+var i : Int = 2
+var prime = "true"
+
+if n == 0 || n == 1 {
+        prime = "false"
+}
+
+else {
+    let sqrtn = Int(floor(pow(Double(n), 0.5))) 
+    while i <= sqrtn {
+	    if n % i == 0 {
+		    prime = "false"
+            break
+	    } else {
+	    	i = i+1
+    	}
+    } 
+} 
+
+print (prime) 
