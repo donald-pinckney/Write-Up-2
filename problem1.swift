24dc93d59f0abaf8e235f9bfab4d5510a2b80630
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-24 18:05:06 +0000

    Add documentation

--- problem1.swift
+++ problem1.swift
@@ -4,13 +4,13 @@ let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
 var i = 0
 var sum = 0
-if input1 < input2 {
+if input1 < input2 {        // Add together from input1 to input2
     i = input1
     while i <= input2 {
         sum = sum + i
         i = i + 1
     }
-} else {
+} else {                    // Add together from input2 to input1
     i = input2
     while i <= input1 {
         sum = sum + i
