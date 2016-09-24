f4aaed4d6a9c41885430db502d59b5ac3510dd18
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-24 18:07:53 +0000

    Add documentation

--- problem2.swift
+++ problem2.swift
@@ -4,10 +4,12 @@ import Foundation
 let y = Double(readLine()!)!
 var x = y - 0.5
 var i = 0
+// Repeat bablyonian method 13 times
 while i <= 13 {
     x = 0.5 * (x + y / x)
     i = i + 1
 }
+// Fix negative sign if y = 0
 if y == 0 {
     x = 0
 }
