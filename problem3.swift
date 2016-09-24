398c0a0a0a8dcb197da470521cadea659efef7e9
Glenn LeBlanc <gl3nnleblanc@gmail.com>
2016-09-24 18:11:24 +0000

    Cleanup

--- problem3.swift
+++ problem3.swift
@@ -1,8 +1,10 @@
 // See problem3.README
 import Foundation
+
 let n = Int(readLine()!)!
 var i = 2
 var isPrime = true
+// Check the remainder of n divided by all numbers less than sqrt(n) and break if any divide evenly
 testPrime: while i < Int(sqrt(Double(n))) {
     if n % i == 0 {
         isPrime = false
@@ -11,9 +13,8 @@ testPrime: while i < Int(sqrt(Double(n))) {
         i = i + 1 
     }
 }
+// Set 0 and 1 to nonprime
 if n == 0 || n == 1 {
  isPrime = false
 }
-
-
 print(isPrime)
