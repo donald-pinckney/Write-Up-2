7fabc9c1127751dd238ebc737ec863ca98ea2c18
Ionatan Soule <ionatan98@gmail.com>
2016-09-25 15:22:13 -0700

    Solved 1 and 2

--- problem1.swift
+++ problem1.swift
@@ -1,5 +1,24 @@
 // See problem1.README
 
-let input1 = Int(readLine()!)!
-let input2 = Int(readLine()!)!
+var input1 = Int(readLine()!)!
+var input2 = Int(readLine()!)!
 
+var sum = 0
+
+if input1 == input2 {
+	print(input1)
+} 
+else if input1 < input2 {
+	while input1 <= input2 {
+		sum = sum + input1
+		input1 = input1 + 1
+	}
+	print(sum)
+}
+else {
+	while input2 <= input1 {
+		sum = sum + input2
+		input2 = input2 + 1
+	}
+	print(sum)
+}
--- problem2.swift
+++ problem2.swift
@@ -2,7 +2,20 @@
 import Foundation
 
 let y = Double(readLine()!)!
+var x = y / 2.0
+var count = 0
+
+if y == 0.0 {
+	x = 0
+} 
+else {
+	while count <= 15 {
+		x = 0.5 * (x + (y / x))
+		count = count + 1
+	}
+}
 
 // Use this to output only 4 decimal digits:
 print(String.localizedStringWithFormat("%.4f", x))
+
 
