88d53e89b80f9f5b489d2ba52fb35675a70723d3
Ionatan Soule <ionatan98@gmail.com>
2016-09-25 15:58:24 -0700

    'Solved' problem 3 - fails bc of time restriction

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,20 @@
 // See problem3.README
 
-let n = Int(readLine()!)!
+let n = Int(readLine()!)! 
+var prime = true
+var count = 0
+
+if n == 0 || n == 1 {
+	prime = false
+}
+
+while prime {
+	for index in 2...(n / 2) {
+		if n % index == 0 {
+			prime = false
+			break 	
+		}
+	}
+}
+
+print(prime)
