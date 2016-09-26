1f05b5e6c7f13b040d576b28216ff65b4a1ffa69
erairdan <ms.hereandthere@gmail.com>
2016-09-26 03:25:00 +0000

    problem3

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,15 @@
 // See problem3.README
 
 let n = Int(readLine()!)!
+
+if n % 2 == 0 {
+	print("false")
+} else if n % 3 == 0 {
+	print("false")
+} else if n % 5 == 0 {
+	print("false")
+} else if n % 7 == 0 {
+	print("false")
+} else {
+	print("true")
+}
