260620b3e41d342f100c18e1fe2c29614c60f96a
Alex Hille <alex_hill20@icloud.com>
2016-09-26 18:00:21 +0000

    Problem 3 commit

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,22 @@
 // See problem3.README
 
-let n = Int(readLine()!)!
+import Foundation
+
+let n = Double(readLine()!)!
+var isPrime = true
+let integerN = Int(floor(n))
+let squareRootN = Int(floor(sqrt(n)))
+
+if integerN != 0 && integerN != 1 && integerN != 2{
+    for index in 2..<squareRootN {
+        if integerN % index == 0 {
+        isPrime = false
+        }
+    }
+} else if integerN == 0 || integerN == 1 {
+        isPrime = false
+} else {
+    isPrime = true
+}
+
+print(isPrime)
