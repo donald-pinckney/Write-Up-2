d9d5f042b519f96a29ca3720b20ee4cb9667e733
jess-zhang <jessie.zh47@gmail.com>
2016-09-26 03:17:37 +0000

    problem 1

--- problem1.swift
+++ problem1.swift
@@ -3,3 +3,16 @@
 let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
 
+var n = 0
+var sum = 0
+
+
+if input2 >= input1 {
+n = ((input2 - input1) + 1) 
+sum = ((input1 + input2) * n) / 2
+} else if input1 > input2 {
+	n = ((input1 - input2) + 1)
+	sum = ((input2 + input1) * n) / 2
+}
+
+print (sum)
