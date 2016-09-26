c07b434aaa0bdb629965837c08f46e75e12aa8ae
erairdan <ms.hereandthere@gmail.com>
2016-09-26 03:28:05 +0000

    problem1

--- problem1.swift
+++ problem1.swift
@@ -1,5 +1,19 @@
 // See problem1.README
 
+import Foundation
 let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
+
+var n = 0
+
+var sum = 0
+if input2 >= input1 { 
+	n = ((input2-input1)+1)
+	sum = (n*(input1+input2)/2)
+	print(sum)
+} else if input1 > input2 {
+	n = ((input1-input2)+1)
+	sum = (n*(input1+input2)/2)
+	print(sum)
+}
 
