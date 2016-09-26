c1121912050669d577ba35f1ef0b934316a5247d
javaraser <darting.thoughts@gmail.com>
2016-09-26 23:31:33 +0000

    Solved Problem1

--- problem1.swift
+++ problem1.swift
@@ -1,5 +1,21 @@
 // See problem1.README
 
-let input1 = Int(readLine()!)!
-let input2 = Int(readLine()!)!
+let y1 = readLine()!
+let x1 = Int(y1)!
+let y2 = readLine()!
+let x2 = Int(y2)!
+var out = 0
 
+if x1 <= x2{
+    out = x1
+    while x1 - x2 != 0 {
+        out += (x1 + 1)
+    }
+    print(out)
+}else{ //if x1 >= x2
+    out = x2
+    while x2 - x1 != 0 {
+        out += (x2 + 1)
+    }
+    print(out)
+} 
