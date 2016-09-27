5ccebaa2bac11cfd0971d13a7d3ea218d25d75aa
Alex Hille <alex_hill20@icloud.com>
2016-09-27 02:38:21 +0000

    correction to problem1

--- problem1.swift
+++ problem1.swift
@@ -6,18 +6,20 @@ var num1 = input1
 var num2 = input2
 var sum = 0
 
-if num2 > num1{
+if input2 > input1{
     while num2 >= num1{
     sum = sum + num1
     num1 = num1 + 1
     }
 }
 
-else if num1 > num2{
+else if input1 > input2{
     while num1 >= num2{
     sum = sum + num2
     num2 = num2 + 1
     }
 }
-
+else {
+    sum = input1
+}
 print(sum)
