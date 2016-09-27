2c3bd3d90fb7a7a2753ef64de08815ef5e06fdf4
javaraser <darting.thoughts@gmail.com>
2016-09-27 04:41:40 +0000

    Problem 3 by Livy Taylor

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,20 @@
 // See problem3.README
+import Foundation
 
 let n = Int(readLine()!)!
+let sqrtn = sqrt(n)
+var prime = false
+var counter = 2
+
+if n == 0 || n == 1{
+    prime = false
+}else{
+    while counter <= sqrtn{
+        if n % counter == 0{
+            prime = true
+        }else{
+            counter += 1
+        }    
+    }
+}
+print(prime)
