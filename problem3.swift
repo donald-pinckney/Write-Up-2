2463a14f9b3a3f088a6b89924bea4b03ced95767
Whyhow314 <whyhow3.141@gmail.com>
2016-09-27 07:41:16 +0000

    problem 3 done, still working on 2

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,23 @@
 // See problem3.README
 
+import Foundation
+
 let n = Int(readLine()!)!
+
+var tried = 2
+
+var prime = true
+
+if n == 0 || n == 1 {
+  prime = false
+}
+else {
+  while Double(tried) < sqrt(Double(n)) {
+    if n % tried == 0 {
+      prime = false
+    }
+    tried = tried + 1
+  }
+}
+
+print(prime)
