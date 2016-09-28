1c8309fabdf882c2d09a2a591cd876b61057714a
niraqw15 <niraqw@gmail.com>
2016-09-28 04:35:07 +0000

    Solve problem 1

--- problem1.swift
+++ problem1.swift
@@ -3,3 +3,15 @@
 let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
 
+let maxVal = max(input1, input2)
+let minVal = min(input1, input2)
+
+var countNum = minVal
+var sum = minVal
+
+while countNum < maxVal {
+	countNum += 1
+	sum += countNum
+}
+
+print(sum)
