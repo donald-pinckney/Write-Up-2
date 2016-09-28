9aaa19055c6c0decfdb52f2433d87486cb880eaf
rellimylime <ermiller95616@gmail.com>
2016-09-28 06:39:12 +0000

    finished problem 1

--- problem1.swift
+++ problem1.swift
@@ -3,3 +3,19 @@
 let input1 = Int(readLine()!)!
 let input2 = Int(readLine()!)!
 
+var start = 0
+var max = 1
+if input1 > input2 {
+    start = input2
+    max = input1
+}else{
+    start = input1
+    max = input2
+}
+var sum = 0
+while start <= max {
+    sum = sum + start
+    start = start + 1
+}
+print(sum) 
+    
