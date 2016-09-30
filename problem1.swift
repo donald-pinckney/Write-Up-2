1afaa527dd7ad67606bbcfd4dd0bc7106ff45c74
javaraser <darting.thoughts@gmail.com>
2016-09-30 03:56:41 +0000

    Problem1 Updated

--- problem1.swift
+++ problem1.swift
@@ -1,22 +1,18 @@
 // See problem1.README
 
-let x1 = Int(readLine()!)! //2 
-let x2 = Int(readLine()!)! //8
+var x1 = Int(readLine()!)! 
+var x2 = Int(readLine()!)! 
 var out = 0
-if x1 = x2{
+if x1 == x2{
     print(x1 * 2)
 }else if x1 > x2{
-    out = x2 //
-    for i in 0...(x1 - x2) { //
-        out += (x2 + i) //
-        print(out)//TEST
+    for i in 0...(x1 - x2) {
+        out += (x2 + i) 
     }
     print(out)
 }else if x1 < x2{
-    out = x2 // 8
-    for i in 0...(x2 - x1) { //6
-        out += (x2 - i) //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-        print(out)//TEST
+    for i in 0...(x2 - x1) {
+        out += (x2 - i)//oihf;oihbfierbg reoiwugve;rb 
     }
     print(out)
 } 
