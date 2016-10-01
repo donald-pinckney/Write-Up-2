125b690c4279d19af432c6a010810436f638d024
Nathan Solomon <nathansolomon1678@gmail.com>
2016-10-01 21:56:54 +0000

    did write up 2

--- problem3.swift
+++ problem3.swift
@@ -1,3 +1,19 @@
 // See problem3.README
+import Foundation
 
-let n = Int(readLine()!)!
+let input = Double(readLine()!)!
+let n: Int = Int(input)
+let squareRoot = sqrt(input)
+let lastFactorToTest: Int = Int(squareRoot)
+var isPrime = true
+
+if lastFactorToTest > 1 {
+	for i in 1...lastFactorToTest {
+		if n % i == 0 && i != 1 {
+			isPrime = false
+		}
+	}
+} else {
+	isPrime = false
+}
+print (isPrime)
